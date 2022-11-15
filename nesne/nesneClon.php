b7e41c011498edb26469524d1c43622e203460c7
Serdıl Umut EREN <73448131+llwusill@users.noreply.github.com>
2022-11-15 18:15:50 +0300

    nesne çalışmaları

---
+++ nesne/nesneClon.php
@@ -0,0 +1,16 @@
+<?php
+// localhost:yourPORTisHERE/nesne/nesneClon.php
+  $object1 = new User();
+  $object1->name = "Alice";
+  $object2 = clone $object1;
+  $object2->name = "Amy";
+
+  echo "object1 name = ".$object1->name."<br>";
+  echo "object2 name = ".$object2->name;
+
+    class User
+      {
+        public $name;
+      }
+
+?>
